a922414e61105b56502cd76a3d5eebd22a41ce69
yaroslav <y2konovalov@mail.ru>
2026-09-13 17:03:25 +0500

    Text has been added to the help section, information has been added to READ_ME.

--- handlers_menu.py
+++ handlers_menu.py
@@ -2,15 +2,31 @@ import telebot
 from keyboards import main_menu
 
 def register_menu_handlers(bot):
+    TEXT_START="Привет! Я NBA Scout - помогу следить за расписанием и любимыми командами."
+    TEXT_HELP=f'''
+    Вот что я умею:\n
+    /start - Перезапустить бота
+    /schedule - Расписание матчей лиги\n
+    /cancel - Выход из любого текущего сценария и возвращение в меню\n
+    /table, /t - Турнирная таблица NBA\n
+    /conf - Турнирная таблица конференции\n
+    /schedule - Расписание матчей лиги\n
+    /favorite - Команды, за которыми наблюдает пользователь\n
+    /subscribe - Подписка на команду\n
+    /unsubscribe - Отписка на команду\n
+    /stats - Посмотреть статистику игрока\n\n
+    Если остались другие вопросы по работе бота - пишите в поддержку: @y2konovalov 
+'''
+    
     @bot.message_handler(commands=['start'])
     def handle_start(message):
         bot.send_message(message.chat.id,
-                          'Привет! Я NBA Scout - помогу следить за расписанием и любимыми командами.', 
+                          TEXT_START, 
                           reply_markup=main_menu())
     
     @bot.message_handler(commands=['help'])
     def handle_help(message):
-        bot.send_message(message.chat.id, 'Вот что я умею:\n/schedule — расписание\n...')
+        bot.send_message(message.chat.id, TEXT_HELP)
 
 
 
